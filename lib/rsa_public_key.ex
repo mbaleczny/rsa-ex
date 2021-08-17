785cc9aed0fc917acc1d660cbc92ea90ae008a9d
Mariusz Bałęczny <mariuszbaleczny@gmail.com>
2021-08-17 15:04:58 +0200

    chore: remove deprecations and format code

--- lib/rsa_public_key.ex
+++ lib/rsa_public_key.ex
@@ -1,17 +1,17 @@
 defmodule RsaEx.RSAPublicKey do
-
   defstruct version: nil,
             public_exponent: nil,
             public_modulus: nil
 
   @type t :: %RsaEx.RSAPublicKey{
-    version: atom,
-    public_exponent: integer,
-    public_modulus: integer
-  }
+          version: atom,
+          public_exponent: integer,
+          public_modulus: integer
+        }
 
   def from_sequence(rsa_key_seq) do
-    %RsaEx.RSAPublicKey{} |> struct(
+    %RsaEx.RSAPublicKey{}
+    |> struct(
       public_exponent: elem(rsa_key_seq, 1),
       public_modulus: elem(rsa_key_seq, 2)
     )
@@ -20,11 +20,13 @@ defmodule RsaEx.RSAPublicKey do
   def as_sequence(rsa_public_key) do
     case rsa_public_key do
       %RsaEx.RSAPublicKey{} ->
-        {:ok, {
-          :RSAPublicKey,
-          rsa_public_key.public_exponent,
-          rsa_public_key.public_modulus,
-        }}
+        {:ok,
+         {
+           :RSAPublicKey,
+           rsa_public_key.public_exponent,
+           rsa_public_key.public_modulus
+         }}
+
       _ ->
         {:error, "invalid RsaEx.RSAPublicKey: #{rsa_public_key}"}
     end
